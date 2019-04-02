a8e781c3963517d5272af3d1e38c84b10b095bb4
madej-lukasz <madel@interia.pl>
2019-04-02 20:58:34 +0200

    Modul 17

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -0,0 +1,72 @@
+package com.kodilla.hibernate.task;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+@Entity
+@Table(name="TASKS")
+public final class Task {
+    private int id;
+    private String description;
+    private Date created;
+    private int duration;
+    public TaskFinancialDetails taskFinancialDetails;
+
+    public Task() {
+    }
+
+
+    public Task(String description, int duration) {
+        this.description = description;
+        this.created = new Date();
+        this.duration = duration;
+    }
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "Id", unique = true)
+
+    public int getId() {
+        return id;
+    }
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+    @NotNull
+    @Column(name = "CREATED")
+
+    public Date getCreated() {
+        return created;
+    }
+    @Column(name = "DURATION")
+    public int getDuration() {
+        return duration;
+    }
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "TASKS_FINANCIALS ID")
+    public TaskFinancialDetails getTaskFinancialDetails(){
+        return taskFinancialDetails;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
+        this.taskFinancialDetails = taskFinancialDetails;
+    }
+}
